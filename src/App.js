fb268d7594d0d39b97bda4ddeaf317a5a141811b
adra3n <srtc.kcgl@gmail.com>
2023-09-22 11:59:17 +0300

    refactored with new logic

--- src/App.js
+++ src/App.js
@@ -1,69 +1,82 @@
 import './app.css'
 import Task from './components/Task'
-import TaskHookForm from './components/TaskHookForm'
+import TaskForm from './components/TaskForm'
 import PeopleForm from './components/PeopleForm'
 import { toast } from 'react-toastify'
 import { useDispatch, useSelector } from 'react-redux'
 import {
   addTask,
-  completeTask,
   removeTask,
   updateTask,
   addPerson,
+  updateTaskOrder,
 } from './redux/actions'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import io from 'socket.io-client'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
-let socket
+let socket = null
 
 function App() {
-  const tasks = useSelector((state) => state.tasks)
+  const columns = useSelector((state) => state.tasks)
   const team = useSelector((state) => state.team)
   const dispatch = useDispatch()
+
   const handleTaskSubmit = (newTask) => {
     dispatch(addTask(newTask))
-    toast.success(`Task: "${newTask.title}" added`)
-    socket.broadcast.emit('task added', newTask)
+    toast.success(`Task: ${newTask.title} added`)
+    socket.emit('task added', newTask)
   }
   const handlePeopleSubmit = (newPerson) => {
     dispatch(addPerson(newPerson))
-    toast.success(`New person "${newPerson}" is added!`)
-    socket.broadcast.emit('people added', newPerson)
+    toast.success(`New person ${newPerson} is added!`)
+    socket.emit('people added', newPerson)
   }
-  const handleComplete = (id) => {
-    const task = tasks.find((t) => t.id === id)
 
-    if (task) {
-      dispatch(completeTask(id))
-      toast.success(`Congrats! "${task.title}" is done!`)
-      socket.broadcast.emit('task completed', id, task)
-    }
+  const handleRemove = (taskId) => {
+    dispatch(removeTask(taskId))
+    toast(`Task with id:${taskId} is removed!`)
+    socket.emit('task removed', taskId)
   }
-  const handleRemove = (id) => {
-    dispatch(removeTask(id))
-    toast(`Task with id:${id} is removed!`)
-    socket.broadcast.emit('task removed', id)
-  }
+
   const handleOnDragEnd = (result) => {
-    if (!result.destination) return
+    if (!result.destination) {
+      return
+    }
 
     const { source, destination } = result
 
-    if (source.droppableId === destination.droppableId) return
+    //moving between columns//
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumnId = source.droppableId
+      const destColumnId = destination.droppableId
+      const taskId = result.draggableId
 
-    const newTasks = [...tasks]
+      //im finding the task object in the source column based on taskId to send it
+      const sourceTask = columns[sourceColumnId].tasks.find(
+        (task) => task.id === taskId
+      )
 
-    const draggedTask = newTasks.splice(source.index, 1)[0]
-    draggedTask.status = destination.droppableId
-    newTasks.splice(destination.index, 0, draggedTask)
+      //dispatching action
+      dispatch(updateTask(taskId, { ...sourceTask, column: destColumnId }))
 
-    dispatch(updateTask(newTasks))
-    socket.broadcast.emit('task updated', draggedTask.id, draggedTask)
+      //emiting event
+      socket.emit('task moved', { taskId, sourceColumnId, destColumnId })
+    } else {
+      //reordering within the same column //
+      const columnId = source.droppableId
+      const sourceIndex = source.index
+      const destIndex = destination.index
 
-    console.log(source, destination)
+      // dispatching
+      dispatch(updateTaskOrder(columnId, sourceIndex, destIndex))
+
+      //emiting
+      socket.emit('task reordered', { columnId, sourceIndex, destIndex })
+    }
   }
 
+  //listening events with socket.io
   useEffect(() => {
     socket = io('http://localhost:4000')
     socket.on('task added', (task) => {
@@ -71,33 +84,26 @@ function App() {
       dispatch(addTask(task))
       toast.success(`Task: "${task.title}" added`)
     })
-    socket.on('task completed', (id, task) => {
-      console.log('websocket:task completed', id, task)
-      dispatch(completeTask(id))
-      toast.success(`Congrats! "${task.title}" is done!`)
-    })
-
     socket.on('people added', (newPerson) => {
       console.log('websocket:person added', newPerson)
       dispatch(addPerson(newPerson))
       toast.success(`New person "${newPerson}" is added!`)
     })
-
     socket.on('task updated', (id, updatedTask) => {
       console.log('websocket:task updated', updatedTask)
       dispatch(updateTask(id, updatedTask))
       toast(`"${updatedTask.title}" is updated`)
     })
-    socket.on('task removed', (id) => {
+    socket.on('task removed', (taskId, columnId) => {
       console.log('websocket:task removed')
-      dispatch(removeTask(id))
-      toast(`Task with id:${id} is removed!`)
+      dispatch(removeTask(taskId, columnId))
+      toast(`Task removed!`)
     })
     return () => {
       console.log('websocket closed')
       socket.disconnect()
     }
-  }, [dispatch])
+  }, ['http://localhost:4000'])
 
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
@@ -105,121 +111,75 @@ function App() {
         <div className="bg-white  max-w-[25vw] flex-shrink-0 flex-none border-r-2 border-solid border-[#f3d4b0] overflow-auto">
           <div className="pt-8 pr-8 pb-6 pl-8 border-b border border-solid border-[#ddd]">
             <h2 className="pb-2 text-2xl">New Task</h2>
-            <TaskHookForm people={team} submitFn={handleTaskSubmit} />
+            <TaskForm people={team} submitFn={handleTaskSubmit} />
           </div>
           <div className="pt-8 pr-8 pb-6  pl-8 border-b border border-solid border-[#ddd]">
             <h2>New Person</h2>
+
             <PeopleForm people={team} submitFn={handlePeopleSubmit} />
           </div>
         </div>
         <div className="flex-1 flex justify-center flex-wrap gap-8 pt-8 pr-8 pb-6 overflow-auto">
-          <div className="flex-1 min-w-[240px] max-w-[360px]">
-            <h2 className="text-2xl pl-3">To Do</h2>
-
-            <Droppable droppableId="todo">
-              {(provided) => (
-                <div {...provided.droppableProps} ref={provided.innerRef}>
-                  {tasks
-                    .filter((t) => t.status === 'todo')
-                    .map((task, index) => (
+          {Object.values(columns).map((column) => (
+            <div key={column.id} className="flex-1 min-w-[240px] max-w-[360px]">
+              <h2 className="text-2xl pl-3">{column.title}</h2>
+              <Droppable droppableId={column.id} key={column.id}>
+                {(provided, snapshot) => (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      // changing color if its droppable spot
+                      background: snapshot.isDraggingOver
+                        ? 'lightblue'
+                        : 'bisque',
+                      padding: 4,
+                      minHeight: 500,
+                    }}
+                  >
+                    {column.tasks.map((task, index) => (
                       <Draggable
                         key={task.id}
-                        draggableId={task.id.toString()}
+                        draggableId={task.id}
                         index={index}
                       >
-                        {(provided) => (
+                        {(provided, snapshot) => (
                           <div
                             {...provided.draggableProps}
                             {...provided.dragHandleProps}
                             ref={provided.innerRef}
+                            style={{
+                              userSelect: 'none',
+                              padding: 16,
+                              margin: '0 0 8px 0',
+                              minHeight: '50px',
+                              //changing active item color here
+                              backgroundColor: snapshot.isDragging
+                                ? '#263B4A'
+                                : '#456C86',
+                              color: 'white',
+                              ...provided.draggableProps.style,
+                            }}
                           >
                             <Task
                               key={task.id}
                               taskObj={task}
-                              onComplete={handleComplete}
                               onRemove={handleRemove}
                             />
                           </div>
                         )}
                       </Draggable>
                     ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          </div>
-          <div className="flex-1 min-w-[240px] max-w-[360px]">
-            <h2 className="text-2xl pl-3">In Progress</h2>
-            <Droppable droppableId="inprogress">
-              {(provided) => (
-                <div {...provided.droppableProps} ref={provided.innerRef}>
-                  {tasks
-                    .filter((t) => t.status === 'inprogress')
-                    .map((task, index) => (
-                      <Draggable
-                        key={task.id}
-                        draggableId={task.id.toString()}
-                        index={index}
-                      >
-                        {(provided) => (
-                          <div
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            ref={provided.innerRef}
-                          >
-                            <Task
-                              key={task.id}
-                              taskObj={task}
-                              onComplete={handleComplete}
-                              onRemove={handleRemove}
-                            />
-                          </div>
-                        )}
-                      </Draggable>
-                    ))}
-
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          </div>
-          <div className="flex-1 min-w-[240px] max-w-[360px]">
-            <h2 className="text-2xl pl-3">Completed</h2>
-            <Droppable droppableId="done">
-              {(provided) => (
-                <div {...provided.droppableProps} ref={provided.innerRef}>
-                  {tasks
-                    .filter((t) => t.status === 'done')
-                    .map((task, index) => (
-                      <Draggable
-                        key={task.id}
-                        draggableId={task.id.toString()}
-                        index={index}
-                      >
-                        {(provided) => (
-                          <div
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            ref={provided.innerRef}
-                          >
-                            <Task
-                              key={task.id}
-                              taskObj={task}
-                              onComplete={handleComplete}
-                              onRemove={handleRemove}
-                            />
-                          </div>
-                        )}
-                      </Draggable>
-                    ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          </div>
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </div>
+          ))}
         </div>
       </div>
     </DragDropContext>
   )
 }
+
 export default App
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,20 +1,83 @@
 import { combineReducers } from 'redux'
-const tasksReducer = (state = [], action) => {
+import { initialColumns } from '../data'
+
+const tasksReducer = (state = initialColumns, action) => {
   switch (action.type) {
     case 'ADD_TASK':
-      return [...state, action.payload]
-    case 'COMPLETE_TASK':
-      return state.map((task) =>
-        task.id === action.payload ? { ...task, status: 'done' } : task
-      )
+      const { newTask } = action.payload
+      return {
+        ...state,
+        ['todo']: {
+          ...state['todo'],
+          tasks: [...state['todo'].tasks, newTask],
+        },
+      }
+
     case 'REMOVE_TASK':
-      return state.filter((task) => task.id !== action.payload)
+      const { taskId: taskIdRemove } = action.payload
+      //i fixed the state mutation error by creating a newState
+      const newState = {}
+
+      for (const columnId in state) {
+        const column = { ...state[columnId] }
+
+        column.tasks = column.tasks.filter((task) => task.id !== taskIdRemove)
+        newState[columnId] = column
+      }
+
+      return newState
 
     case 'UPDATE_TASK':
-      const { id, updatedTask } = action.payload
-      return state.map((task) =>
-        task.id === id ? { ...task, ...updatedTask } : task
+      const { taskId: taskIdUpdate, updatedTask } = action.payload
+
+      //source column id
+      const sourceColumnId = Object.keys(state).find((columnId) =>
+        state[columnId].tasks.find((task) => task.id === taskIdUpdate)
       )
+      //destination column id
+      const destColumnId = updatedTask.column
+
+      const sourceTasks = [...state[sourceColumnId].tasks]
+      const destTasks = [...state[destColumnId].tasks]
+
+      //using find since for the task id since its only 1
+      const taskToUpdate = sourceTasks.find((task) => task.id === taskIdUpdate)
+      //im removing the task from the old (source) column
+      sourceTasks.splice(sourceTasks.indexOf(taskToUpdate), 1)
+      //im adding it to new column
+      destTasks.push(updatedTask)
+
+      console.log(destTasks, updatedTask, sourceTasks, taskToUpdate)
+
+      //update state
+      return {
+        ...state,
+        [sourceColumnId]: {
+          ...state[sourceColumnId],
+          tasks: sourceTasks,
+        },
+        [destColumnId]: {
+          ...state[destColumnId],
+          tasks: destTasks,
+        },
+      }
+
+    case 'UPDATE_TASK_ORDER':
+      const { columnId: columnIdOrder, sourceIndex, destIndex } = action.payload
+      //getting tasks with the columnId
+      const tasks = [...state[columnIdOrder].tasks]
+      //removing task from the source with sourceIndex
+      const [movedTask] = tasks.splice(sourceIndex, 1)
+      //adding it to destIndex with splice
+      tasks.splice(destIndex, 0, movedTask)
+
+      return {
+        ...state,
+        [columnIdOrder]: {
+          ...state[columnIdOrder],
+          tasks,
+        },
+      }
 
     default:
       return state
